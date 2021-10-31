14da233c325fb51ec2ff8250d4c74a0e46eb0245
Hrishikesh <hrishikeshpramodnair@gmail.com>
2021-10-31 15:35:10 +0530

    Changed GUI to python

---
+++ interface.py
@@ -0,0 +1,88 @@
+# img_viewer.py
+
+import PySimpleGUI as sg
+import os.path
+import converter
+
+
+beingConverted = False
+filename = ''
+
+# First the window layout in 2 columns
+file_list_column = [
+    [
+        sg.Text("Image Folder"),
+        sg.In(size=(25, 1), enable_events=True, key="-FOLDER-"),
+        sg.FolderBrowse(),
+    ],
+    [
+        sg.Listbox(
+            values=[], enable_events=True, size=(40, 20), key="-FILE LIST-"
+        )
+    ],
+]
+
+# For now will only show the name of the file that was chosen
+image_viewer_column = [
+    [sg.Text(key="-MESSAGE-")],
+    [sg.Text(size=(40, 1), key="-TOUT-")],
+    [sg.Image(key="-IMAGE-")],
+]
+
+# ----- Full layout -----
+layout = [
+    [
+        sg.Column(file_list_column),
+        sg.Button('Convert'),
+        sg.VSeperator(),
+        sg.Column(image_viewer_column),
+    ]
+]
+
+window = sg.Window("Image Viewer", layout)
+
+# Run the Event Loop
+while True:
+    event, values = window.read()
+    if event == "Exit" or event == sg.WIN_CLOSED:
+        break
+    # Folder name was filled in, make a list of files in the folder
+    if event == 'Convert':
+        beingConverted = True
+        window["-MESSAGE-"].update('Converting Img to 3D model...')
+        converter.convertImage(filename)
+        print('Done')
+        beingConverted = False
+        window["-MESSAGE-"].update('Choose an image from list on the left')
+    if event == "-FOLDER-":
+        if beingConverted == True:
+            window["-MESSAGE-"].update('Converting Img to 3D model...')
+            pass
+        folder = values["-FOLDER-"]
+        try:
+            # Get list of files in folder
+            file_list = os.listdir(folder)
+        except:
+            file_list = []
+
+        fnames = [
+            f
+            for f in file_list
+            if os.path.isfile(os.path.join(folder, f))
+            and f.lower().endswith((".png", ".gif","jpg","jpeg"))
+        ]
+        window["-FILE LIST-"].update(fnames)
+    elif event == "-FILE LIST-":  # A file was chosen from the listbox
+        if beingConverted == True:
+            window["-MESSAGE-"].update('Converting Img to 3D model...')
+            pass
+        try:
+            filename = os.path.join(
+                values["-FOLDER-"], values["-FILE LIST-"][0]
+            )
+            window["-TOUT-"].update(filename)
+            window["-IMAGE-"].update(filename=filename)
+        except:
+            pass
+
+window.close()
